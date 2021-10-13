e139d8cf3a3363ebba578509a3e981aa6c489af2
qazmko19 <87934971+qazmko19@users.noreply.github.com>
2021-10-13 19:44:11 +0300

    Update settings.py

--- Django/Вступление в Django/Домашнее задание/Домашнее/settings.py
+++ Django/Вступление в Django/Домашнее задание/Домашнее/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ygmo^z0yfrb*qc8i&pajm=us@(+7)p5wry6c)ov^3t&j2@cei5'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
